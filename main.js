18b92cc5344ed06067a1cd64b985d85c6c5b5c89
Sushmita-Jacob <sushmita.cjacob@gmail.com>
2025-10-22 02:21:48 +0000

    Part 1 Section 3

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 const input = document.getElementById('input');
-function frequency(pitch) {
+// create web audio api elements
+const audioCtx = new AudioContext();
+const gainNode = audioCtx.createGain();
+// create Oscillator node
+const oscillator = audioCtx.createOscillator();
+oscillator.connect(gainNode);
+gainNode.connect(audioCtx.destination);
+oscillator.type = "sine";
 
+oscillator.start();
+gainNode.gain.value = 0;
+function frequency(pitch) {
+    gainNode.gain.setValueAtTime(100, audioCtx.currentTime)
+    oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime)
+    gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 1)
 }
 function handle() {
+    audioCtx.resume();
+    gainNode.gain.value = 0;
     frequency(input.value);
 }
