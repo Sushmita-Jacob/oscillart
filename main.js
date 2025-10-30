f86e4ed66048e780d51027cfd30cabe7c4f51fe1
Sushmita-Jacob <sushmita.cjacob@gmail.com>
2025-10-30 21:17:25 +0000

    Part 2 Section 3

--- main.js
+++ main.js
@@ -13,13 +13,29 @@ var ctx = canvas.getContext("2d");
 var width = ctx.canvas.width;
 var height = ctx.canvas.height;
 var amplitude = 40;
+var counter = 0;
+var interval = null;
+
+function drawWave() {
+    ctx.clearRect(0, 0, width, height);
+    x = 0;
+    y = height/2;
+    ctx.moveTo(x, y);
+    ctx.beginPath();
+    counter = 0;
+    interval = setInterval(line, 20);
+}
 
 function line() {
     freq = pitch / 10000;
-    y = height/2 + (amplitude * Math.sin(x * 2 * MathPI * freq));
+    y = height/2 + (amplitude * Math.sin(x * 2 * Math.PI * freq));
     ctx.lineTo(x, y);
     ctx.stroke();
     x = x + 1;
+    counter++;
+    if(counter > 50) {
+        clearInterval(interval);
+    }
 }
 
 oscillator.start();
@@ -37,9 +53,13 @@ function frequency(pitch) {
     oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime)
     gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 1)
 }
+
+
+
 function handle() {
     audioCtx.resume();
     gainNode.gain.value = 0;
     var usernotes = String(input.value);
     frequency(notenames.get(usernotes));
+    drawWave();
 }
