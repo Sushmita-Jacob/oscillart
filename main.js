02e3cd1a354414828bfe165798a8b372f9a233bc
Sushmita-Jacob <sushmita.cjacob@gmail.com>
2025-11-01 01:33:41 +0000

    Part 5 Section 3

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ const color_picker = document.getElementById('color');
 const vol_slider = document.getElementById('vol-slider');
 var blob, recorder = null;
 var chunks = [];
+const recording_toggle = document.getElementById('record');
 
 function drawWave() {
     clearInterval(interval);
@@ -97,4 +98,32 @@ function startRecording(){
     const combinedStream = new MediaStream();
     canvasStream.getVideoTracks().forEach(track => combinedStream.addTrack(track));
     audioDestination.stream.getAudioTracks().forEach(track => combinedStream.addTrack(track));
+    recorder = new MediaRecorder(combinedStream, { mimeType: 'video/webm'});
+    recorder.ondataavailable = e => {
+        if (e.data.size > 0) {
+            chunks.push(e.data);
+        }
+    };
+    recorder.onstop = () => {
+        const blob = new Blob(chunks, { type: 'video/webm' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'recording.webm';
+        a.click();
+        URL.revokeObjectURL(url);
+    }
+    recorder.start();
+}
+
+var is_recording = false;
+function toggle(){
+    is_recording = !is_recording;
+    if(is_recording){
+        recording_toggle.innerHTML = "Stop Recording";
+        startRecording();
+    } else{
+        recording_toggle.innerHTML = "Start Recording";
+        recorder.stop();
+    }
 }
